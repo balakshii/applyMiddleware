7de81f666066cb0d710c1eec2c6d54296abad2c6
Vladyslav balakshii <vladyslav.balakshii@gmail.com>
2021-02-17 17:47:19 +0200

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import './index.css';
+import rootReducer from './reducers';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// use applyMiddleware to add the thunk middleware to the store
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
